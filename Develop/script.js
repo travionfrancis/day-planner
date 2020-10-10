e13eb6e30303d02d5fa0f55475668ba750ff58ec
TraviFravi <travi1500@gmail.com>
2020-10-09 23:03:31 -0400

    More psuedo code added, as I'll have to plan this out, it seems to be a challenge

--- Develop/script.js
+++ Develop/script.js
@@ -1,2 +1,10 @@
 //Pseudocode
 //
+// Use moment.js to start doing this HW, already linked.description
+
+// Plans
+// when the page loads it shows the current hour
+// Gonna need the user's time. also make the current hour green
+//
+// When the time area is clicked, the user enters in a sentence detailing their planned day.
+// the blue button on the right allows the sentence to be saved.
