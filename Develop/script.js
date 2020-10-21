aeb4b76b9f2bbb1fc00debc314a7d16adc7eaddc
TraviFravi <travi1500@gmail.com>
2020-10-21 00:14:27 -0400

    Added some local storage capabilities

--- Develop/script.js
+++ Develop/script.js
@@ -8,3 +8,14 @@
 //
 // When the time area is clicked, the user enters in a sentence detailing their planned day.
 // the blue button on the right allows the sentence to be saved.
+
+// local storage, so look up the documentation
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
