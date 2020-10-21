a1c0a76710bafcfbb5aa8581d349015830aa9257
TraviFravi <travi1500@gmail.com>
2020-10-21 01:45:26 -0400

    Added some more local storage functionality

--- Develop/script.js
+++ Develop/script.js
@@ -19,3 +19,14 @@ $("#hour14 .description").val(localStorage.getItem("hour14"));
 $("#hour15 .description").val(localStorage.getItem("hour15"));
 $("#hour16 .description").val(localStorage.getItem("hour16"));
 $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+$(document).ready(function () {
+  $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+  $(".saveBtn").on("click", function () {
+    console.log(this);
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+
+    localStorage.setItem(time, text);
+  });
+});
